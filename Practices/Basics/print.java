97b2f8292cd53dbb9bf4f0d46dab8c40ed60b271
Saai Krahaanth S Ja <126404799+SaaiKrahaanth@users.noreply.github.com>
2026-04-11 10:54:16 +0530

    last

---
+++ Practices/Basics/print.java
@@ -0,0 +1,19 @@
+class java{
+    public static void main(String args[]){
+        System.out.println("sd");
+        final int s=55;
+        System.out.println(s);
+        String str=new String("saai");
+        System.out.println(str);
+        int len=str.length();
+        System.out.println(len);
+        int i=0;
+        System.out.println((int)Math.ceil(len/2.0));
+        while(i<Math.ceil(len/2)){
+            char temp=str.charAt(i);
+            str.indexOf(i) =str.indexOf(len-1)
+            i++;
+        }
+
+    }
+}
